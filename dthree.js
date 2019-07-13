2aeb67c7abec88ba76fa06490293d7a21765d310
Liam Diprose <liam@liamdiprose.com>
2019-07-13 14:12:39 +1200

    Move to html

--- dthree.js
+++ dthree.js
@@ -1,23 +1,33 @@
 
 
+let game = d3.select("body")
 
 function frame() {
 
-    let game = d3.select("#game")
         // .selectAll("circle")
         // .text(function (d) { return d; });
 
-    let circles = game.selectAll("circle")
+    let circles = game.selectAll(".virus")
         .data(["red", "blue", "orange"])
         //.enter()
         //.append("circle")
+        .style("fill", (color) => color)
         .transition()
-        .attr("cx", () => Math.random() * 400)
-        .attr("cy", () => Math.random() * 400)
-        .attr("r", () => 3)
-        .style("fill", (color) => color);
+        .style("top", () => '' + Math.random() * 400 + 'px')
+        .style("left", () => '' + Math.random() * 400 + 'px')
+        //.attr("r", () => 10);
 
     requestAnimationFrame(frame);
 }
 
 requestAnimationFrame(frame);
+
+
+var dragHandler = d3.drag()
+    .on("drag", function () {
+        d3.select(this)
+            .attr("cx", d3.event.x)
+            .attr("cy", d3.event.y);
+    });
+
+let newCircle = dragHandler(game.select("#new-circle"));
